448bf866abd06a22b5517a51dfac810d3c18d4b7
Skinny001 <bamigboyetemitope84@gmail.com>
2024-10-14 17:56:55 +0100

    playvideo added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = ({setSidebar}) => {
   return (
@@ -15,9 +16,11 @@ const Navbar = ({setSidebar}) => {
             <img className='menu-icon' onClick={()=>setSidebar(prev=>prev===false?true:false)}
                 src={menu_icon} alt="" 
             />
-            <img className='logo' 
-                src={logo} alt="" 
-            />
+            <Link to='/'>
+                <img className='logo' 
+                    src={logo} alt="" 
+                />
+            </Link>
         </div>
 
         <div className='nav-middle flex-div'>
--- src/data.js
+++ src/data.js
@@ -1,1 +1,14 @@
-export const API_KEY = 'AIzaSyCSghvVeJgX0TAj_cz32Msd2d8XHv4zaiw';
+
+export const value_converter = (value) =>{
+    if(value>=1000000)
+        {
+            return Math.floor(value/1000000)+ "M";
+        }
+        else if(value>=1000)
+            {
+                return Math.floor(value/1000)+ "K";
+            }
+            else{
+                return value;
+            }
+}
